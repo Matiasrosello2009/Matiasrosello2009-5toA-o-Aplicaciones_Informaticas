bc1cc156e656bf4fc34e8d6bc3b442be46b7b873
alumno <alumno@Instituto.local>
2025-03-18 10:01:08 -0300

    repaso 2.0

--- repaso.js
+++ repaso.js
@@ -2,3 +2,11 @@
 let añoNacimiento = 2009 
 let añoActual = 2025 
 alert('Mi edad es', añoActual - añoNacimiento)
+
+let edad = Number(prompt('Qué edad tenes?')) 
+
+if (edad >= 17){
+    alert('Podés ingresar')
+} else {
+    alert ('No podés ingresas')
+}
